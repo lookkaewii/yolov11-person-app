f8410adf4c34f75d2d70c4887f7d31ce912a6f4c
lookkaewii <naruemon_phenc@hotmail.co.th>
2025-09-21 14:52:31 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,19 +10,18 @@ model = YOLO("yolo11n.pt")
 uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg","jpeg", "png"])
 
 if uploaded_image is not None:
-# Show original image
-st.image(uploaded_image , caption="Uploaded Image", use_container_width=True)
-# Read image and convert to numpy array
-image = Image.open(uploaded_image)
-image_np = np.array(image)
-# Run YOLO inference
-st.info("Running YOLO object detection...")
-results = model.predict(image_np , conf=0.4)
-# Draw results on image
-result_image = results[0].plot()
-st.image(result_image , caption="YOLO Detection Result", use_container_width=
-True)
-st.success("Detection completed!")
+    # Show original image
+    st.image(uploaded_image , caption="Uploaded Image", use_container_width=True)
+    # Read image and convert to numpy array
+    image = Image.open(uploaded_image)
+    image_np = np.array(image)
+    # Run YOLO inference
+    st.info("Running YOLO object detection...")
+    results = model.predict(image_np , conf=0.4)
+    # Draw results on image
+    result_image = results[0].plot()
+    st.image(result_image , caption="YOLO Detection Result", use_container_width=True)
+    st.success("Detection completed!")
 
 # Extract detection results
 boxes = results[0].boxes
